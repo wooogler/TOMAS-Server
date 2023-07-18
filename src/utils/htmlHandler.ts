6611dedd19050a7544cd7387c088ccd12aa4b23e
ma-h-m <72591218+ma-h-m@users.noreply.github.com>
2023-07-18 11:49:09 -0400

    Update htmlHandler.ts
    Convert TOMAS-PYTHON changes into Typescript

--- src/utils/htmlHandler.ts
+++ src/utils/htmlHandler.ts
@@ -1,5 +1,4 @@
 import { JSDOM } from "jsdom";
-import { Page } from "puppeteer";
 
 const removeSpecificTags = (element: Element, tagNames: string[]) => {
   for (const tagName of tagNames) {
@@ -220,8 +219,13 @@ type ConditionType =
   | "button"
   | "input_button"
   | "input_text"
+  | "input_checkbox"
+  | "input_range"
   | "link"
-  | "list";
+  | "table"
+  | "paragraph"
+  | "list"
+  | "Unknown";
 
 interface Condition {
   type: ConditionType;
@@ -230,36 +234,77 @@ interface Condition {
 
 const conditions: Condition[] = [
   {
-    type: "input_radio",
-    test: (element: Element) => {
-      const radioInputs = element.querySelectorAll('input[type="radio"]');
-      return radioInputs.length >= 2;
-    },
-  },
-  {
     type: "button",
     test: (element: Element) => {
       return (
-        element.tagName.toLowerCase() === "button" &&
-        element.getAttribute("type") === "button"
+        element.tagName.toLowerCase() === "button" 
+        //&& element.getAttribute("type") === "button" // some buttons do not have type attribute
       );
     },
+  },
+  {
+    type: "input_text",
+    test: (element: Element) => {
+        return (
+            element.tagName.toLowerCase() === "input" &&
+            element.getAttribute("type") === "text"
+        )
+    }
+  },
+  {
+    type: "input_radio",
+    test: (element: Element) => {
+        return (
+            element.tagName.toLowerCase() === "input" &&
+            element.getAttribute("type") === "radio"
+        );
+        }
   },
   {
     type: "input_button",
     test: (element: Element) => {
       return (
-        element.tagName.toLowerCase() === "div" &&
-        element.querySelector('input[type="button"]') !== null &&
-        element.querySelector("label") !== null
+        element.tagName.toLowerCase() === "input" &&
+        element.getAttribute("type") === "button"
       );
     },
   },
+  {
+    type: "input_checkbox",
+    test: (element: Element) => {
+        return (
+            element.tagName.toLowerCase() === "input" &&
+            element.getAttribute("type") === "checkbox"
+        );
+        }
+  },
+  {
+    type: "input_range",
+    test: (element: Element) => {
+        return (
+            element.tagName.toLowerCase() === "input" &&
+            element.getAttribute("type") === "range"
+        );
+        }
+  },
+  
   {
     type: "link",
     test: (element: Element) => {
       return element.tagName.toLowerCase() === "a";
     },
+  },
+  {
+    type: "table",
+    test: (element: Element) => {
+        return element.tagName.toLowerCase() === "table";
+        }
+  },
+  {
+    type: "paragraph",
+    test: (element: Element) => {
+        return element.tagName.toLowerCase() === "p";
+        }
   },
   {
     type: "list",
@@ -271,6 +316,12 @@ const conditions: Condition[] = [
       );
     },
   },
+  {
+    type: "Unknown",
+    test: (element: Element) => {
+        return true;
+        }
+  }
 ];
 
 function getCondition(element: Element): ConditionType | null {
@@ -282,52 +333,143 @@ function getCondition(element: Element): ConditionType | null {
   return null;
 }
 
+
+
+function get_condition(element: Element): string | null {
+    for (const condition of conditions) {
+      if (condition.test(element)) {
+        return condition.type;
+      }
+    }
+    return null;
+  }
+
+  function traverse_element(element: Element, s: Set<string>): void {
+    const iAttr = element.getAttribute("i");
+    if (iAttr !== null) {
+      s.add(iAttr);
+    }
+    else{
+        return;
+    }
+    for (const child of element.children) {
+      traverse_element(child, s);
+    }
+  }
+
+const threshold = 10;
+function traverse_element2(
+    element: Element,
+    s: Element[],
+    action_components: Set<string>
+  ): void {
+    const iAttr = element.getAttribute("i");
+    if (iAttr !== null) {
+      const tmp_leave_node_set = new Set<string>();
+      traverse_element(element, tmp_leave_node_set);
+      const inter_set = new Set([...tmp_leave_node_set].filter((x) => action_components.has(x)));
+      if (inter_set.size === 0) {
+        return;
+      }
+      if (inter_set.size <= threshold) {
+        s.push(element);
+        return;
+      }
+    }
+  
+    for (const child of element.children) {
+      traverse_element2(child, s, action_components);
+    }
+  }
+  
+  
+
 export interface ActionComponent {
   html: string;
   type: string;
   i: string;
 }
-
 export function extractActionComponents(html: string): ActionComponent[] {
-  const dom = new JSDOM(html);
-  const document = dom.window.document;
-  const root = document.body;
-
-  let components: ActionComponent[] = [];
-  let candidateElements: Element[] = querySelectorAllReverseBFS(
-    root,
-    "div, button"
-  );
-  let addedIValues = new Set<string | null>();
-
-  for (let i = 0; i < candidateElements.length; i++) {
-    const conditionType = getCondition(candidateElements[i]);
-    if (conditionType) {
-      const currentIValue = candidateElements[i].getAttribute("i");
-
-      if (addedIValues.has(currentIValue)) continue;
-      let descendants = candidateElements[i].querySelectorAll("*");
-      let skip = false;
-      descendants.forEach((descendant) => {
-        if (addedIValues.has(descendant.getAttribute("i"))) {
-          skip = true;
-        }
-      });
-      if (skip) continue;
-
-      if (currentIValue) {
-        components.push({
-          html: candidateElements[i].outerHTML,
-          type: conditionType,
-          i: currentIValue,
-        });
-        addedIValues.add(currentIValue);
-      }
+    const dom = new JSDOM(html);
+    const document = dom.window.document;
+    const root = document.body;
+  
+    const tmp_root = root.cloneNode(true) as Element;
+    const list_elements = Array.from(tmp_root.querySelectorAll("ul, ol"));
+    const added_i_values = new Set<string>();
+    for (const list_element of list_elements) {
+        traverse_element(list_element, added_i_values);
     }
+
+    const table_elements = Array.from(tmp_root.querySelectorAll("table"));
+    for (const i of table_elements) {
+      traverse_element(i, added_i_values);
+    }
+  
+    // get fieldset elements
+    const fieldset_elements = Array.from(tmp_root.querySelectorAll("fieldset"));
+    for (const i of fieldset_elements) {
+      traverse_element(i, added_i_values);
+    }
+
+    // find all paragraph elements with interactive elements
+    const paragraph_elements = Array.from(tmp_root.querySelectorAll("p"));
+    const paragraph_with_active_elements: Element[] = [];
+    for (const i of paragraph_elements) {
+      const tmp_rec = i.querySelectorAll("input, button, a, select, textarea");
+      if (i.getAttribute("i") && added_i_values.has(i.getAttribute("i")!)) {
+          continue;
+      }
+          
+      if (tmp_rec.length > 0) {
+          paragraph_with_active_elements.push(i);
+          traverse_element(i, added_i_values);
+      }
   }
 
-  return components;
-}
+    // get all other action components
+    const interactive_elements = Array.from(tmp_root.querySelectorAll("input, button, a, select, textarea"));
+    const interactive_elements_i_attr = new Set(
+        interactive_elements
+          .map((el) => el.getAttribute("i"))
+          .filter((x) => x !== null) as string[]
+      );
+    
+    // remove duplicated elements from interactive elements
+    const tmp_set = new Set(interactive_elements);
+    for (const i of tmp_set) {
+    if (i.getAttribute("i") && added_i_values.has(i.getAttribute("i")!)) {
+        interactive_elements.splice(interactive_elements.indexOf(i), 1);
+    }
+    }
+
+ interactive_elements.push(...list_elements, ...table_elements, ...fieldset_elements, ...paragraph_with_active_elements);
+  const interactive_elements_i_attr2 = new Set(
+      interactive_elements
+      .map((el) => el.getAttribute("i"))
+      .filter((x) => x !== null) as string[]
+  ); 
+    // merge components
+    // For each element containing less than {threshold} action components, we try to merge them.
+
+    const merged_components: Element[] = [];
+    traverse_element2(tmp_root, merged_components, interactive_elements_i_attr2);
+
+    const components: any[] = [];
+
+    for (const element of merged_components) {
+        const condition_type = get_condition(element);
+        if (condition_type) {
+                components.push({
+                html: element.outerHTML,
+                type: condition_type,
+                i: element.getAttribute("i"),
+            });
+        }
+    }
+
+    return components;
+  }
 
 interface ComponentNode {
   element?: Element;
