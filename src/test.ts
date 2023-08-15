36fd71eff9ad987e84ccfa2a8ad306f1d4b12d10
Sangwook Lee <leesang627@gmail.com>
2023-08-15 17:41:16 -0400

    update the prompt for getSelectInfo

--- src/test.ts
+++ src/test.ts
@@ -60,6 +60,9 @@ ${questions.join("\n")}
   logScreenResult(
     await pageHandler.navigate("https://www.greyhound.com", true)
   );
+  logScreenResult(
+    await pageHandler.click(".hcr-btn-7-6-0.hcr-btn--primary-7-6-0.lKKy1", true)
+  );
   // logScreenResult(await pageHandler.click("#dateInput-from", true));
   // logScreenResult(await pageHandler.select(".hcr-clndr-7-6-0.wnaY8", true));
   // logScreenResult(await pageHandler.click("#searchInputMobile-from", false));
